5810f94dcb05943815cb14f03134a2708084e3b1
AlvinBirley <alvinbirley@gmail.com>
2024-11-07 12:25:57 -0600

    Complete project by wiring clear div to clear calc display.

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ const numData = numDivs.dataset;
 //Select all operator divs
 const opDivs = document.querySelectorAll('.op');
 //Select clear div
-const clearDiv = document.querySelector('.clear');
+const clear = document.querySelector('.clear');
 //Select screen div 
 const screen = document.querySelector('.screen')
 //Select equals div
 const equal = document.querySelector('.equal')
 //Select point div
 const point = document.querySelector('.point')
+//Variable to hold "." string
+const pointStr = "."
 //Variable for first component of the equation 
 let firstNum = ''
 //Variable for the operator of the equation 
 let operator = ''
+//Variable for innerHTML of screen div
+let display = screen.innerHTML
 
 //functions for each operator 
 const add = (a,b) => a+b
@@ -34,9 +38,24 @@ numDivs.forEach(num => {
     num.addEventListener('click', (event) => {
         const numVal = getNumValue(event);
         firstNum += numVal
-        
+        screen.innerHTML += numVal
     });
 });
+
+function addPoint (){
+    firstNum += pointStr
+    screen.innerHTML += pointStr
+}
+
+point.addEventListener("click", addPoint)
+
+function clearAll (){
+    screen.innerHTML = ''
+    firstNum = ''
+    operator = ''
+}
+
+clear.addEventListener("click", clearAll)
 
 //Event listener that splits the 
 opDivs.forEach(op => {
@@ -44,19 +63,20 @@ opDivs.forEach(op => {
         const opVal = getNumValue(event);
         firstNum += "%"
         operator+= opVal
+        screen.innerHTML += opVal
     });
 });
 
-//Function to split firstNum into two components 
+//Function to solve equation
 document.querySelector('.equal').addEventListener('click', function() {
     let array = String(firstNum).split("")
     let firstIndex = array.indexOf('%')
     let firstComp = Number(array.slice(0, firstIndex).join(''))
     let secondComp = Number(array.slice(firstIndex+1).join(''))
-    return operator=="/" ? console.log(divide(firstComp,secondComp))
-    : operator=="*"  ? console.log(multiply(firstComp,secondComp))
-    : operator=="-"  ? console.log(subtract(firstComp,secondComp))
-    : console.log(add(firstComp,secondComp));
+    return operator=="/" ? screen.innerHTML=divide(firstComp,secondComp)
+    : operator=="*"  ? screen.innerHTML=multiply(firstComp,secondComp)
+    : operator=="-"  ? screen.innerHTML=subtract(firstComp,secondComp)
+    : screen.innerHTML=add(firstComp,secondComp)
 });
 
 
