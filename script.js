33999c8fc929cacb7fa57b66f500c30ec6f2917d
AlvinBirley <alvinbirley@gmail.com>
2024-11-06 22:00:14 -0600

    Link function that completes equation to equals button

--- script.js
+++ script.js
@@ -10,19 +10,24 @@ const clearDiv = document.querySelector('.clear');
 const screen = document.querySelector('.screen')
 //Select equals div
 const equal = document.querySelector('.equal')
+//Select point div
+const point = document.querySelector('.point')
 //Variable for first component of the equation 
 let firstNum = ''
 //Variable for the operator of the equation 
 let operator = ''
 
+//functions for each operator 
+const add = (a,b) => a+b
+const subtract = (a,b) => a-b
+const multiply = (a,b) => a*b
+const divide = (a,b) => a/b
 
 //Function to return the data-value of number divs when clicked
 function getNumValue(event){
     const dataValue = event.currentTarget.dataset.value;
     return dataValue
 }
-
-
 
 //Event listener that console logs the data-values of number divs 
 numDivs.forEach(num => {
@@ -42,51 +47,16 @@ opDivs.forEach(op => {
     });
 });
 
+//Function to split firstNum into two components 
+document.querySelector('.equal').addEventListener('click', function() {
+    let array = String(firstNum).split("")
+    let firstIndex = array.indexOf('%')
+    let firstComp = Number(array.slice(0, firstIndex).join(''))
+    let secondComp = Number(array.slice(firstIndex+1).join(''))
+    return operator=="/" ? console.log(divide(firstComp,secondComp))
+    : operator=="*"  ? console.log(multiply(firstComp,secondComp))
+    : operator=="-"  ? console.log(subtract(firstComp,secondComp))
+    : console.log(add(firstComp,secondComp));
+});
 
 
-
-
-
-function testing(){
-    console.log(firstNum)
-    console.log(operator)
-} 
-
-
-equal.addEventListener('click', testing)
-
-//WHAT I CAN TRY IS RETURNING THE FIRST COMPONENT AS THE RETURN EVENT OF A FUNCTION - ALSO, LOOK BACK AT THE RPS PROJECT
-//ANOTHER THING YOU COULD DO IS HAVE THE OPERATOR BUTTONS INSERT A SPACE OR A CERTAIN CHARACTER INTO YOUR VARIABLE - THEN YOU CAN SIMPLY SPLIT IT
-
-// variables
-let a = 6
-let b = 11
-let o = "+"
-
-//functions for each operator 
-const add = (a,b) => a+b
-
-const subtract = (a,b) => a-b
-
-const multiply = (a,b) => a*b
-
-const divide = (a,b) => a/b
-
-console.log(add(a,b))
-console.log(subtract(a,b))
-console.log(multiply(a,b))
-console.log(divide(a,b))
-
-//function that returns solutions
-function operate(a,b,o){
-    return o=="*"? multiply(a,b)
-    :o=="+"? add(a,b)
-    :o=="-"? subtract(a,b)
-    :divide(a,b)
-}
-
-console.log(operate(a,b,o))
-
-function fullNumber (fistNum) {
-console.log(firstNum)
-}
